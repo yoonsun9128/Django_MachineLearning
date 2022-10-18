89e7156745f569cbd2fe2c5c6bff7ab4ace099e8
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-10-18 18:09:40 +0900

    2022_10_18 with gyuhyeon_ machine learning yolov5 model을 사용하여 DB 저장 및 프론트에 이미지 띄우기 기능 구현

--- user/views.py
+++ user/views.py
@@ -15,7 +15,7 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('/home')
+            return redirect('/')
         else:
             return redirect('user/login')
     else:
