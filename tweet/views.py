be16119d67384973875fdbe151eb44b6382ff067
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-10-18 21:07:08 +0900

    2022_10_18_ category front 기능 구현 완료

--- tweet/views.py
+++ tweet/views.py
@@ -9,16 +9,12 @@ def home(request):
     if request.method == 'GET': 
         user = request.user.is_authenticated
         if user:
-            # 추가할 것 : 업로드한 이미지 보여주기(view_img), 이미지를 인식한 카테고리(img_catagory) 
             temp = TweetModel.objects.values('upload_img')
             temp2 = TweetModel.objects.values('upload_label_img')
-            result = zip(temp, temp2)
-            print(temp)
-        
-            # all_upload_img = [x['upload_img'] for x in temp]
+            category = TweetModel.objects.values('category')
+            result = zip(temp, temp2, category)
             
-            # return render(request, 'tweet/home.html', {'upload_img': all_upload_img})
-            return render(request, 'tweet/home.html', {'total_img':result})
+            return render(request, 'tweet/home.html', {'total_img': result})
         else:
             return redirect('/login')
             
