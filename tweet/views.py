238dc2708f765f967f85f1578331b157a3d03a5f
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-10-18 12:56:53 +0900

    2022_10_18 이미지 업로드 하기 구현 완료

--- tweet/views.py
+++ tweet/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from . import tests
 from django.contrib.auth import authenticate
 from .models import TweetModel
-from Django_AI.settings import MEDIA_ROOT,MEDIA_URL
+
 # Create your views here.
 
 def home(request):
@@ -10,24 +10,25 @@ def home(request):
         user = request.user.is_authenticated
         if user:
             # 추가할 것 : 업로드한 이미지 보여주기(view_img), 이미지를 인식한 카테고리(img_catagory) 
-            return render(request, 'tweet/home.html')
+            temp = TweetModel.objects.values('upload_img')
+            print(temp)
+        
+            # all_upload_img = [x['upload_img'] for x in temp]
+            
+            # return render(request, 'tweet/home.html', {'upload_img': all_upload_img})
+            return render(request, 'tweet/home.html', {'upload_img':temp})
         else:
             return redirect('/login')
             
     elif request.method == 'POST':
-        print(request.POST)
         user = request.user.is_authenticated
         tweet = TweetModel()
-        img_upload = request.POST['img_upload'] #이미지 업로드 기능,
-        
-        img_log, img_category = tests.change_img()   
 
-        tweet.img_upload = img_upload
-        tweet.img_category = img_category
-        tweet.img_log = img_log
-        
-        for i in range(10):
-            print(img_upload, img_category, img_log)
+        upload_img = request.FILES['upload_img']
+
+        tweet.upload_img = upload_img
+        # tweet.category = 1
+        # tweet.upload_label_img = 2
 
         tweet.save()
         
@@ -37,6 +38,3 @@ def home(request):
         # }
         return render(request, 'tweet/home.html')
         # https://stackoverflow.com/questions/64002542/how-to-use-yolov5-model-in-django   -> stackoverflow django,yolov5 연동하는 코드
-
-
-
